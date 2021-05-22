4382d93c9c3352f5d9f32a591b0cfdebee01b00b
Bruno <bruno.nakayabu@gmail.com>
2021-05-21 21:56:39 -0300

    dev: #5 node-cache package added to project. 20s Cache implemented

--- api/src/controllers/CardsController.ts
+++ api/src/controllers/CardsController.ts
@@ -2,11 +2,15 @@ import { GithubService } from '../services/GithubService'
 import { Request, Response } from 'express'
 import { Pagination } from '../models/GithubApi'
 
+const NodeCache = require('node-cache')
+
+const cache = new NodeCache()
 const service = new GithubService()
 
 export default {
   get: async (req: Request, res: Response) => {
     const { page, perPage, org, language } = req.query
+    const cacheKey = process.env.CACHE_KEY
 
     const pagination: Pagination = {
       page: page as string,
@@ -15,14 +19,23 @@ export default {
       sort: 'created'
     }
 
-    const data = await service
-      .getOrgRepositoriesByLanguage(String(org), String(language), pagination)
+    const cacheResponse = cache.get(cacheKey)
 
-    if (data != null) {
-      return res.status(200)
-        .json({ data })
+    if (cacheResponse ===  undefined) {
+      const data = await service
+        .getOrgRepositoriesByLanguage(String(org), String(language), pagination)
+
+      if (data != null) {
+        cache.set(cacheKey, data, 20)
+
+        return res.status(200)
+          .json({ data })
+      }
+
+      return res.status(400)
     }
 
-    return res.status(400)
+    return res.status(200)
+      .json({ data: cacheResponse })
   }
 }
