59e4600648e4c6f1cf273f338dfa44e4c6e94bde
Bruno <bruno.nakayabu@gmail.com>
2021-05-22 13:46:53 -0300

    dev: change enpoint route

--- api/index.ts
+++ api/index.ts
@@ -12,7 +12,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', CardsController.get)
+app.get('/cards', CardsController.get)
 
 app.listen(PORT, () => {
   console.log(`Listenning on port ${PORT}`)
